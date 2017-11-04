5f42356ab9fa397b17f299e9d5481d621dfab6aa
Tektanis <littlej3800@gmail.com>
2017-11-04 14:19:03 -0400

    added initial game objects.

---
+++ src/game/SingeltonsCreator.java
@@ -0,0 +1,15 @@
+package game;
+
+import game.gameObjects.Submarine;
+
+public class SingeltonsCreator {
+    static Submarine playerSubmarine;
+
+    public static Submarine getOrCreatePlayerSubmarineFactoryMethod() {
+        if(playerSubmarine == null) {
+            //TODO: give actual starting values based on size of map
+            playerSubmarine = new Submarine(0, 0);
+        }
+        return playerSubmarine;
+    }
+}
